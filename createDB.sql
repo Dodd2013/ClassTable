fba90cadfe994c8c4d9c26aeea4b1271e6a01cfb
Dodd <dodd@dodd2014.com>
2016-03-12 16:04:49 +0800

    修改数据库

--- createDB.sql
+++ createDB.sql
@@ -58,46 +58,39 @@ create table tb_sub_detail(
 	CONSTRAINT `SUB_DET` FOREIGN KEY (`sub_id`) REFERENCES `tb_submit` (`sub_id`),
 	CONSTRAINT `DET_PRO` FOREIGN KEY (`pro_id`) REFERENCES `tb_problems` (`pro_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
-create table tb_resume(
-	username varchar(40) primary key,
-	chname varchar(40),
-	ginder varchar(10),
-	birth varchar(40),
-	phone varchar(40),
-	address varchar(200),
-	province varchar(20),
-	city varchar(20),
-	town varchar(20),
-	career varchar(100),
-	degree varchar(100),
-	track varchar(40),
-	project varchar(200),
-	skill varchar(40),
-	joblocation1 varchar(20),
-	joblocation2 varchar(20),
-	joblocation3 varchar(20),
-	filepath varchar(255),
-	constraint fk foreign key(username) references tb_users(user_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-create table tb_job(
-	title varchar(40),
-	description varchar(200),
-	comname varchar(40),
-	location varchar(40),
-	jstatus varchar(40),
-	user_email varchar(100)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 create table tb_batchs(
 	batch_id int primary key auto_increment,
 	batch_name varchar(50) not null,
 	batch_yeas int not null
 )
-create table tb_semester{
+create table tb_semester(
 	sem_begintime date not null,
 	sem_endtime data not null,
 	sem_name varchar(50)
-}
-create table student(){
-	
-}
+)
+create table tb_student(
+	batch_id int not null,
+	roll_id varchar(40) primary key not null,
+	chinese_name varchar(40) not null,
+	english_name varchar(40) not null
+	CONSTRAINT `STU_BAT` FOREIGN KEY (`batch_id`) REFERENCES `tb_batchs` (`batch_id`)
+)
+create table tb_teacher(
+	teacher_id int primary key auto_increment,
+	teacher_name varchar(40) not null
+)
+create table tb_class(
+	class_id int primary KEY auto_increment,
+	teacher_id int,
+	class_name varchar(100) not null,
+	class_locations varchar(),
+	class_week
+	CONSTRAINT `TEA_CLA` FOREIGN KEY (`teacher_id`) REFERENCES `tb_teacher` (`teacher_id`)
+)
+create table tb_batch_class(
+	class_id int not null,
+	batch_id int not null,
+	CONSTRAINT `bat_cla` FOREIGN KEY (`class_id`) REFERENCES `tb_class` (`class_id`),
+	CONSTRAINT `BAT_CLA` FOREIGN KEY (`batch_id`) REFERENCES `tb_batchs` (`batch_id`)
+)
+create table 
