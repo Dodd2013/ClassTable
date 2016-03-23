540a7c988b3af93bc39e99838de2cfcb497b6019
Dodd <291106637@qq.com>
2016-03-24 07:28:56 +0800

    修改表

--- createDB.sql
+++ createDB.sql
@@ -58,39 +58,71 @@ create table tb_sub_detail(
 	CONSTRAINT `SUB_DET` FOREIGN KEY (`sub_id`) REFERENCES `tb_submit` (`sub_id`),
 	CONSTRAINT `DET_PRO` FOREIGN KEY (`pro_id`) REFERENCES `tb_problems` (`pro_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
 create table tb_batchs(
 	batch_id int primary key auto_increment,
 	batch_name varchar(50) not null,
 	batch_yeas int not null
-)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 create table tb_semester(
+	sem_id int primary key not null,
 	sem_begintime date not null,
-	sem_endtime data not null,
+	sem_endtime date not null,
+	sem_status int null,
 	sem_name varchar(50)
-)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 create table tb_student(
-	batch_id int not null,
-	roll_id varchar(40) primary key not null,
-	chinese_name varchar(40) not null,
-	english_name varchar(40) not null
+		batch_id int not null,
+	st_roll_id varchar(40) primary key not null,
+	st_chinese_name varchar(40) not null,
+	st_english_name varchar(40) not null,
 	CONSTRAINT `STU_BAT` FOREIGN KEY (`batch_id`) REFERENCES `tb_batchs` (`batch_id`)
-)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 create table tb_teacher(
 	teacher_id int primary key auto_increment,
 	teacher_name varchar(40) not null
-)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 create table tb_class(
+	sem_id int not null,
 	class_id int primary KEY auto_increment,
 	teacher_id int,
 	class_name varchar(100) not null,
-	class_locations varchar(),
-	class_week
+	class_locations varchar(100),
+	CONSTRAINT `SEM_CLA` FOREIGN KEY (`sem_id`) REFERENCES `tb_semester` (`sem_id`),
 	CONSTRAINT `TEA_CLA` FOREIGN KEY (`teacher_id`) REFERENCES `tb_teacher` (`teacher_id`)
-)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 create table tb_batch_class(
 	class_id int not null,
 	batch_id int not null,
 	CONSTRAINT `bat_cla` FOREIGN KEY (`class_id`) REFERENCES `tb_class` (`class_id`),
-	CONSTRAINT `BAT_CLA` FOREIGN KEY (`batch_id`) REFERENCES `tb_batchs` (`batch_id`)
-)
-create table 
+	CONSTRAINT `cla_bat` FOREIGN KEY (`batch_id`) REFERENCES `tb_batchs` (`batch_id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create table tb_class_time(
+	ct_id int primary key auto_increment not null,
+	class_id int not null,
+	ct_week int not null,
+	ct_weekday int not null,
+	ct_daytime varchar(20),
+	CONSTRAINT `time_class` FOREIGN KEY (`class_id`) REFERENCES `tb_class` (`class_id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create table tb_user_student(
+	st_roll_id varchar(40) not null,
+	user_id varchar(40) not null,
+	CONSTRAINT `st_user` FOREIGN KEY (`st_roll_id`) REFERENCES `tb_student` (`st_roll_id`),
+	CONSTRAINT `user_st` FOREIGN KEY (`user_id`) REFERENCES `tb_users` (`user_id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create table tb_user_teacher(
+	teacher_id int not null,
+	user_id varchar(40) not null,
+	CONSTRAINT `tc_user` FOREIGN KEY (`teacher_id`) REFERENCES `tb_teacher` (`teacher_id`),
+	CONSTRAINT `user_tc` FOREIGN KEY (`user_id`) REFERENCES `tb_users` (`user_id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create table tb_check_student(
+	ct_id int not null,
+	st_roll_id varchar(40) not null,
+	ckst_check int null,	##为空则没有到，
+	ckst_reason varchar(200) null,
+	CONSTRAINT `ck_ct` FOREIGN KEY (`ct_id`) REFERENCES `tb_class_time` (`ct_id`),
+	CONSTRAINT `ck_st` FOREIGN KEY (`st_roll_id`) REFERENCES `tb_student` (`st_roll_id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
